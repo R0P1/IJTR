8798694348c1c8162e74ec3cee1b5f77451a57db
R0P1 <139617581+R0P1@users.noreply.github.com>
2023-08-29 22:21:57 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,19 @@
+# Instal John The Ripper
+
+instal(){
+    sudo apt update
+    sudo apt install build-essential libssl-dev yasm pkg-config
+    git clone https://github.com/openwall/john.git
+    cd john
+    ./configure && make -s clean && make -sj4
+}
+
+nanya(){
+    read -p "Apakah anda mau menginstal John The Ripper [iya/tidak]: " nanya
+
+    if [[ ${nanya} == "iya" ]]; then
+        instal
+        exit 0
+    else
+        exit 1
+}
