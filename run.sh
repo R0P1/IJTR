ce44e010a7d8023c20a6a978ef7d4bc91881721c
R0P1 <139617581+R0P1@users.noreply.github.com>
2023-08-29 23:25:49 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 instal(){
     path="/root/instal-john-the-ripper/john/run"
     cd /root
-    sudo apt update
-    sudo apt-get install build-essential libssl-dev zlib1g-dev yasm pkg-config libgmp-dev libpcap-dev libbz2-dev
+    apt update
+    apt-get install build-essential libssl-dev zlib1g-dev yasm pkg-config libgmp-dev libpcap-dev libbz2-dev
     git clone https://github.com/openwall/john.git
     cd john/src
     ./configure && make -s clean && make -sj4
