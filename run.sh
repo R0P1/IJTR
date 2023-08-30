228451c53fe1cfeb1764af3ff239a1547173cd58
R0P1 <139617581+R0P1@users.noreply.github.com>
2023-08-30 20:18:21 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,25 +2,30 @@
 # Instal John The Ripper
 
 instal(){
-    path="/root/john/run"
-    cd /root
-    apt update
-    apt-get install build-essential libssl-dev zlib1g-dev yasm pkg-config libgmp-dev libpcap-dev libbz2-dev
-    git clone https://github.com/openwall/john.git
-    cd john/src
-    ./configure && make -s clean && make -sj4
-    echo "Selesai."
+    read -p "[$] Masukkan direktori untuk menyimpan John The Ripper: " dir
+    if [[ -d ${dir} ]]; then
+        cd $dir
+        apt update
+        apt-get install build-essential libssl-dev zlib1g-dev yasm pkg-config libgmp-dev libpcap-dev libbz2-dev
+        git clone https://github.com/openwall/john.git
+        cd john/src
+        ./configure && make -s clean && make -sj4
+        echo "[+] Selesai."
+    else
+        echo "[!] Kesalahan: direktori ${dir} tidak ada."
+        exit 1
+    fi
 }
 
 nanya(){
-    read -p "Apakah anda mau menginstal John The Ripper [iya/tidak]: " nanya
+    read -p "[$] Apakah anda mau menginstal John The Ripper [iya/tidak]: " nanya
     if [[ ${nanya} == "iya" ]]; then
         instal
         exit 0
     elif [[ ${nanya} == "tidak" ]]; then
         exit 0
     else
-        echo "Kesalahan: masukkan salah."
+        echo "[!] Kesalahan: masukkan salah."
         exit 1 
     fi
 }
@@ -29,7 +34,7 @@ cek_root(){
     if [ $UID -eq 0 ]; then
         nanya
     else
-        echo "Jalankan sebagai root."
+        echo "[!] Jalankan sebagai root."
     fi
 }
 
