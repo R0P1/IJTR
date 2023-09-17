d2a9a77a2c4dab531ceb1f067e3da2164e7c2767
R0P1 <139617581+R0P1@users.noreply.github.com>
2023-09-17 16:34:15 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,22 +2,14 @@
 # Instal John The Ripper
 
 instal(){
-    read -p "[$] Masukkan direktori untuk menyimpan John The Ripper: " dir
-    if [[ -d ${dir} ]]; then
-        cd $dir
-        apt update
-        apt-get install build-essential libssl-dev zlib1g-dev yasm pkg-config libgmp-dev libpcap-dev libbz2-dev
-        git clone https://github.com/openwall/john.git
-        cd john/src
-        ./configure && make -s clean && make -sj4
-        echo "export PATH=$PATH:$dir/john/run/" >> ~/.bashrc
-        echo "alias john='$dir/john/run/john'" >> ~/.bashrc
-        source ~/.bashrc
-        echo "[+] Selesai."
-    else
-        echo "[!] Kesalahan: direktori ${dir} tidak ada."
-        exit 1
-    fi
+    dir="/usr/share"
+    cd $dir
+    apt update
+    apt-get install build-essential libssl-dev zlib1g-dev yasm pkg-config libgmp-dev libpcap-dev libbz2-dev
+    git clone https://github.com/openwall/john.git
+    cd john/src
+    ./configure && make -s clean && make -sj4
+    echo "[+] Selesai."
 }
 
 nanya(){
